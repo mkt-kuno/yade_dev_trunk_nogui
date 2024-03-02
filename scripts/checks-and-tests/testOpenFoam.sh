88f293244beb862323ab0f3cf907513e1b5401c5
bchareyre <bruno.chareyre@grenoble-inp.fr>
2024-03-02 17:02:09 +0000

    Update testOpenFoam.sh

--- scripts/checks-and-tests/testOpenFoam.sh
+++ scripts/checks-and-tests/testOpenFoam.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-ls -la /root/OpenFOAM/openfoam/etc/bashrc
-source  /root/OpenFOAM/openfoam/etc/bashrc
+ls -la /root/OpenFOAM/OpenFOAM-v1906/etc/bashrc
+source  /root/OpenFOAM/OpenFOAM-v1906/etc/bashrc
 
 cd ..
 rm -rf Yade-OpenFOAM-coupling
