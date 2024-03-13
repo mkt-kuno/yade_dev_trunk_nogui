a37bc8d04642226778d6c86400f1b363849ca635
bchareyre <bruno.chareyre@3sr-grenoble.fr>
2024-03-13 16:05:38 +0100

    use new calling mechanism

--- scripts/checks-and-tests/testOpenFoam.sh
+++ scripts/checks-and-tests/testOpenFoam.sh
@@ -25,6 +25,7 @@ decomposePar
 mkdir yadep
 mkdir spheres
 
-mpiexec --allow-run-as-root -n 1 python3 scriptYade.py : -n 2 icoFoamYade -parallel
-mpiexec --allow-run-as-root -n 4 ../../../install/bin/yade-ci scriptMPI.py : -n 2 icoFoamYade -parallel
+# mpiexec --allow-run-as-root -n 1 python3 scriptYade.py : -n 2 icoFoamYade -parallel
+# mpiexec --allow-run-as-root -n 4 ../../../install/bin/yade-ci scriptMPI.py : -n 2 icoFoamYade -parallel
+mpirun --allow-run-as-root -n 1 python3 scriptYade.py
 
