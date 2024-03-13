cd81d24bb77bd2b669ef9c5df770b7bb90a6234b
bchareyre <bruno.chareyre@3sr-grenoble.fr>
2024-03-13 16:58:42 +0100

    verbosity

--- examples/openfoam/example_icoFoamYade/scriptYade.py
+++ examples/openfoam/example_icoFoamYade/scriptYade.py
@@ -107,8 +107,9 @@ print("OFOAM executable? ",os.path.exists(OFOAM_EXE) )
 
 
 ## FIXME : THE SERIAL COUPLING FAILS! DON'T USE AT THE MOMENT...
-
-initMPI()  #Initialize the mpi environment, always required.
+#print("initMPI()")
+#initMPI()  #Initialize the mpi environment, always required.
+print("yade.FoamCoupling()")
 fluidCoupling = yade.FoamCoupling()
 fluidCoupling.comm = MPI.COMM_WORLD
 
