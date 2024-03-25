b3ddc20fd21562bfa253b3a8de1ba2328ddfcf1a
bchareyre <bruno.chareyre@grenoble-inp.fr>
2024-03-25 19:15:05 +0100

    check only scriptMPI.py

--- scripts/checks-and-tests/testOpenFoam.sh
+++ scripts/checks-and-tests/testOpenFoam.sh
@@ -34,9 +34,9 @@ fi
 
 # mpiexec --allow-run-as-root -n 1 python3 scriptYade.py : -n 2 icoFoamYade -parallel
 # mpiexec --allow-run-as-root -n 4 ../../../install/bin/yade-ci scriptMPI.py : -n 2 icoFoamYade -parallel
-echo "_____ RUNING mpirun --allow-run-as-root -n 1 python3 scriptYade.py"
-sleep 2
-mpirun --allow-run-as-root -n 1 python3 scriptYade.py
+#echo "_____ RUNING mpirun --allow-run-as-root -n 1 python3 scriptYade.py"
+#sleep 2
+#mpirun --allow-run-as-root -n 1 python3 scriptYade.py
 
 echo "_____ RUNING mpirun --allow-run-as-root -n 4 ../../../install/bin/yade-ci scriptMPI.py"
 sleep 2
