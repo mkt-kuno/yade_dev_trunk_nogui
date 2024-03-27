0c3b11134130c5576236f9a95958956317121d7d
bchareyre <bruno.chareyre@3sr-grenoble.fr>
2024-03-27 17:51:06 +0100

    test ubuntu22.04 + ofoam2312

--- scripts/checks-and-tests/testOpenFoam.sh
+++ scripts/checks-and-tests/testOpenFoam.sh
@@ -7,8 +7,8 @@ rm -rf Yade-OpenFOAM-coupling
 if [ -f "/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc" ]; then
     bashrcPath=/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc # compiled version (ubuntu18.04)
     git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
-elif [ -f "/usr/lib/openfoam/openfoam2012/etc/bashrc" ]; then
-    bashrcPath=/usr/lib/openfoam/openfoam2012/etc/bashrc # precompiled package (ubuntu22.04)
+elif [ -f "/usr/lib/openfoam/openfoam2312/etc/bashrc" ]; then
+    bashrcPath=/usr/lib/openfoam/openfoam2312/etc/bashrc # precompiled package (ubuntu22.04)
     git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
 else #assume OFOAM6, use older coupling code
     bashrcPath=/root/OpenFOAM/OpenFOAM-6/etc/bashrc
