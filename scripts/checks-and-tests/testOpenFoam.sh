5f0e6f5044cb5301d96f127a04c9787a1f95fc38
bchareyre <bruno.chareyre@3sr-grenoble.fr>
2024-04-19 16:20:26 +0200

    don't source foam/bashrc if an environment pre-exists

--- scripts/checks-and-tests/testOpenFoam.sh
+++ scripts/checks-and-tests/testOpenFoam.sh
@@ -3,19 +3,22 @@
 cd ..
 rm -rf Yade-OpenFOAM-coupling
 
-
-if [ -f "/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc" ]; then
-    bashrcPath=/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc # compiled version (ubuntu18.04)
-    git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
-elif [ -f "/usr/lib/openfoam/openfoam2312/etc/bashrc" ]; then
-    bashrcPath=/usr/lib/openfoam/openfoam2312/etc/bashrc # precompiled package (ubuntu22.04)
-    git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
-else #assume OFOAM6, use older coupling code
-    bashrcPath=/root/OpenFOAM/OpenFOAM-6/etc/bashrc
+if [ -z "$WM_PROJECT_VERSION" ]; then
+    if [ -f "/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc" ]; then
+        bashrcPath=/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc # compiled version (ubuntu18.04)
+        git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
+    elif [ -f "/usr/lib/openfoam/openfoam2312/etc/bashrc" ]; then
+        bashrcPath=/usr/lib/openfoam/openfoam2312/etc/bashrc # precompiled package (ubuntu22.04)
+        git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
+    else #assume OFOAM6, use older coupling code
+        bashrcPath=/root/OpenFOAM/OpenFOAM-6/etc/bashrc
+        git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
+    fi
+    source $bashrcPath
+else  # some bashrc have been sourced already (e.g. in .gitlab-ci.yml for the pipeline)
     git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
 fi
 
-source $bashrcPath
 cd Yade-OpenFOAM-coupling
 python3 setup.py
 #./Allclean
