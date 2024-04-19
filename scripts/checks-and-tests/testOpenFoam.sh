1f64ec27c6450695889ccca4d46f7f7576adc896
bchareyre <bruno.chareyre@3sr-grenoble.fr>
2024-04-19 18:28:03 +0200

    use foamCoupling code from yade repo

--- scripts/checks-and-tests/testOpenFoam.sh
+++ scripts/checks-and-tests/testOpenFoam.sh
@@ -6,19 +6,16 @@ rm -rf Yade-OpenFOAM-coupling
 if [ -z "$WM_PROJECT_VERSION" ]; then
     if [ -f "/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc" ]; then
         bashrcPath=/root/OpenFOAM/OpenFOAM-v1906/etc/bashrc # compiled version (ubuntu18.04)
-        git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
     elif [ -f "/usr/lib/openfoam/openfoam2312/etc/bashrc" ]; then
         bashrcPath=/usr/lib/openfoam/openfoam2312/etc/bashrc # precompiled package (ubuntu22.04)
-        git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
     else #assume OFOAM6, use older coupling code
         bashrcPath=/root/OpenFOAM/OpenFOAM-6/etc/bashrc
-        git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
     fi
     source $bashrcPath
-else  # some bashrc have been sourced already (e.g. in .gitlab-ci.yml for the pipeline)
-    git clone -b supportOFoam1906 https://gitlab.com/yade-dev/Yade-OpenFOAM-coupling.git
 fi
 
+#git clone https://github.com/dpkn31/Yade-OpenFOAM-coupling.git
+cp -rf trunk/pkg/openfoam/coupling Yade-OpenFOAM-coupling
 cd Yade-OpenFOAM-coupling
 python3 setup.py
 #./Allclean
